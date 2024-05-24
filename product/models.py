909582404781da51536d78528c8f799bff28b4cf
Amit Saini <amitsaini95>
2024-05-24 10:33:08 +0530

    create categroy model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+    slug=models.SlugField(blank=True,max_length=50)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural="Categories"
