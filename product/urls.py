552d1f4b5170f3e7724ff0590bf4ec702979ac29
Amit Saini <amitsaini95>
2024-05-24 13:23:35 +0530

    product details page

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views
+app_name="product"
 urlpatterns=[
-    path('',views.ProductView,name="ProductList")
-    
-    
+    path('',views.ProductView,name="ProductList"), 
+    path('product/<slug:slug>',views.ProductDetailView,name="productDetails") 
 ]
--- product/views.py
+++ product/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,get_object_or_404
 from .models import *
 # Create your views here.
 def ProductView(request):
-    product=Product.objects.all().order_by('-id')[:6]
+    products=Product.objects.all().order_by('-id')[:6]
     context={
-        'product':product
+        'products':products
     }
     return render(request,"base/productlist.html",context)
+def ProductDetailView(request,slug):
+    product=get_object_or_404(Product,slug=slug)
+    context={'product':product}
+    return render(request,"base/productDetails.html",context)
+    
