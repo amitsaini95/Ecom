15f8eed2963bcc9f64de0d5154cae346e54570e3
Amit Saini <amitsaini95>
2024-05-24 10:45:17 +0530

    create product model

--- product/models.py
+++ product/models.py
@@ -15,3 +15,18 @@ class Category(models.Model):
         return self.name
     class Meta:
         verbose_name_plural="Categories"
+class Product(models.Model):
+    name=models.CharField(max_length=60)
+    slug=models.CharField(blank=True,max_length=60)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    desc=models.TextField()
+    price=models.IntegerField()
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.name)
+        return super(Product,self).save(*args,**kwargs)
+    
+    def __str__(self):
+        return self.name
+    
