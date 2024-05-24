3c885db3f4f10f11bc2deeda361727bc5b424e2c
Amit Saini <amitsaini95>
2024-05-24 10:51:29 +0530

    add extra fields in product model

--- product/models.py
+++ product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -18,7 +19,9 @@ class Category(models.Model):
 class Product(models.Model):
     name=models.CharField(max_length=60)
     slug=models.CharField(blank=True,max_length=60)
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    auth=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,null=True,blank=True)
+    productImage=models.ImageField(upload_to="productImage",null=True,blank=True)
     desc=models.TextField()
     price=models.IntegerField()
     created=models.DateTimeField(auto_now_add=True)
