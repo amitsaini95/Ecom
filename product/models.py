a62de7da4c76382b361703c2dd2ea588016e9311
Amit Saini <amitsaini95>
2024-05-24 10:40:08 +0530

    fix create auto  categroy slug

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Category(models.Model):
@@ -6,6 +7,9 @@ class Category(models.Model):
     slug=models.SlugField(blank=True,max_length=50)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.name)
+        return super(Category,self).save(*args,**kwargs)
     
     def __str__(self):
         return self.name
