585d7b3139232c609b15dbc28d1c0d183b6a8fec
Amit Saini <amitsaini95>
2024-05-24 12:26:50 +0530

    create product list and product view

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .import views
 urlpatterns=[
-    path('',views.IndexView,name="Index")
+    path('',views.ProductView,name="ProductList")
+    
+    
 ]
--- product/views.py
+++ product/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import *
 # Create your views here.
-def IndexView(request):
-    return HttpResponse("ok")
+def ProductView(request):
+    product=Product.objects.all().order_by('-id')[:6]
+    context={
+        'product':product
+    }
+    return render(request,"base/productlist.html",context)
