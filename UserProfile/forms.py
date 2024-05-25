110d72e76d7c872534dcf5a300d158d2cca254e7
Amit Saini <amitsaini95>
2024-05-25 13:03:19 +0530

    signup and login page styling

--- UserProfile/forms.py
+++ UserProfile/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth.models import User
@@ -5,7 +6,16 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model=User
         fields=('username','password1','password2')
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            
 class LoginForm(AuthenticationForm):
     username=forms.CharField(max_length=100)
     password=forms.CharField(widget=forms.PasswordInput())
     fields=('username','password')
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
