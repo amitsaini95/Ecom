aa2e7d43803fa3f918da88ad9800af45164ae613
Amit Saini <amitsaini95>
2024-05-26 10:41:39 +0530

    create Cart model

--- product/models.py
+++ product/models.py
@@ -50,9 +50,17 @@ class Product(models.Model):
         except:
             url=''
         return url
-
     
     def __str__(self):
         return self.name
+class Cart(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField()
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.product.name
+
     
     
